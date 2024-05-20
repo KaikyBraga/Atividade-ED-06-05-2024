0b72306785d17e75493633d71f4267f280fb2208
GuilhermeCastilho02 <guilherme222castilho@gmail.com>
2024-05-19 23:58:07 -0300

    modularizando o código para uma main

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <chrono>
 #include <fstream>
-#include "selectionSortUtils.h"
-#include "bubbleSortUtils.h"
-#include "insertionSortUtils.h"
+
+#include "list.h"
+#include "sortsUtils.h"
 
 using namespace std;
+
 using chrono::high_resolution_clock;
 using chrono::duration_cast;
 using chrono::nanoseconds;
@@ -14,17 +15,17 @@ int main()
 {
     // BUBBLE SORT
 
-        int iNumLinhas = 100;
+    int iNumLinhas = 100;
 
     // Inicialização da semente do gerador de números aleatórios com o tempo atual
     srand(time(nullptr)); 
-    ofstream outputFile("bubbleSort_time.csv", ios::out | ios::trunc);
+    ofstream outputFile1("bubbleSort_time.csv", ios::out | ios::trunc);
     
-    outputFile << "Quantidade de Elementos,Tempo Bubble Sort Padrão,Tempo Bubble Sort Otimizado" << endl;
+    outputFile1 << "Quantidade de Elementos,Tempo Bubble Sort Padrão,Tempo Bubble Sort Otimizado" << endl;
 
     Node* head1 = nullptr;
     Node* head2 = nullptr;
-
+   
     for (int i = 1; i <= iNumLinhas ; i ++) 
     {
         addRandomElements(&head1, 10000, i);
@@ -41,76 +42,72 @@ int main()
         auto timeDuration1 = duration_cast<nanoseconds>(timeStop1 - timeStart1);
         auto timeDuration2 = duration_cast<nanoseconds>(timeStop2 - timeStart2);
 
-        outputFile << 10000 << "," << timeDuration1.count() << "," << timeDuration2.count() << endl;
+        outputFile1 << 10000 << "," << timeDuration1.count() << "," << timeDuration2.count() << endl;
         clearList(&head1);
         clearList(&head2);
     }
 
-    outputFile.close();
+    outputFile1.close();
 
     // SELECTION SORT
 
-    int iNumLinhas = 100;
-
     // Inicialização da semente do gerador de números aleatórios com o tempo atual
     srand(time(nullptr)); 
-    ofstream outputFile("selectionSort_time.csv", ios::out | ios::trunc);
+    ofstream outputFile2("selectionSort_time.csv", ios::out | ios::trunc);
     
-    outputFile << "Quantidade de Elementos,Tempo Selection Sort Padrão,Tempo Selection Sort Otimizado" << endl;
+    outputFile2 << "Quantidade de Elementos,Tempo Selection Sort Padrão,Tempo Selection Sort Otimizado" << endl;
 
-    Node* head1 = nullptr;
-    Node* head2 = nullptr;
+    Node* head3 = nullptr;
+    Node* head4 = nullptr;
 
     for (int i = 1; i <= iNumLinhas ; i ++) 
     {
-        addRandomElements(&head1, 10000, i);
-        head2 = copyList(&head1);
+        addRandomElements(&head3, 10000, i);
+        head2 = copyList(&head3);
 
         auto timeStart1 = high_resolution_clock::now();
-        selectionSort(&head1);
+        selectionSort(&head3);
         auto timeStop1 = high_resolution_clock::now();
 
         auto timeStart2 = high_resolution_clock::now();
-        optimizedSelectionSort(&head2);
+        optimizedSelectionSort(&head4);
         auto timeStop2 = high_resolution_clock::now();
 
         auto timeDuration1 = duration_cast<nanoseconds>(timeStop1 - timeStart1);
         auto timeDuration2 = duration_cast<nanoseconds>(timeStop2 - timeStart2);
 
-        outputFile << 10000 << "," << timeDuration1.count() << "," << timeDuration2.count() << endl;
-        clearList(&head1);
-        clearList(&head2);
+        outputFile2 << 10000 << "," << timeDuration1.count() << "," << timeDuration2.count() << endl;
+        clearList(&head3);
+        clearList(&head4);
     }
 
-    outputFile.close();
+    outputFile2.close();
 
     // INSECTION SORT
-    
-        int iNumLinhas = 100;
 
     // Inicialização da semente do gerador de números aleatórios com o tempo atual
     srand(time(nullptr)); 
-    ofstream outputFile("insertSort_time.csv", ios::out | ios::trunc);
+    ofstream outputFile3("insertSort_time.csv", ios::out | ios::trunc);
     
-    outputFile << "Quantidade de Elementos,Tempo (nanossegundos)" << endl;
+    outputFile3 << "Quantidade de Elementos,Tempo (nanossegundos)" << endl;
 
-    Node* head = nullptr;
+    Node* head5 = nullptr;
 
     for (int i = 1; i <= iNumLinhas ; i ++) 
     {
-        addRandomElements(&head, 10000, i);
+        addRandomElements(&head5, 10000, i);
 
         auto timeStart = high_resolution_clock::now();
-        insertSort(&head);
+        insertSort(&head5);
         auto timeStop = high_resolution_clock::now();
 
         auto timeDuration = duration_cast<nanoseconds>(timeStop - timeStart);
 
-        outputFile << 10000 << "," << timeDuration.count() << endl;
-        clearList(&head);
+        outputFile3 << 10000 << "," << timeDuration.count() << endl;
+        clearList(&head5);
     }
 
-    outputFile.close();
+    outputFile3.close();
     
     return 0;
 }
